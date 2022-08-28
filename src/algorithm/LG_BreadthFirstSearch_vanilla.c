809bc84b259bd9410fb33bc17e184fe5c9663e2c
Dmitry Lichmanov <dimlichmanov@gmail.com>
2022-08-28 18:21:35 +0300

    Our simplified version of BFS_vanilla

--- src/algorithm/LG_BreadthFirstSearch_vanilla.c
+++ src/algorithm/LG_BreadthFirstSearch_vanilla.c
@@ -71,83 +71,34 @@ int LG_BreadthFirstSearch_vanilla
     GRB_TRY( GrB_Matrix_nrows (&n, A) );
     LG_ASSERT_MSG (src < n, GrB_INVALID_INDEX, "invalid source node") ;
 
-    // determine the semiring type
-    GrB_Type     int_type  = (n > INT32_MAX) ? GrB_INT64 : GrB_INT32 ;
-    GrB_BinaryOp
-        second_op = (n > INT32_MAX) ? GrB_SECOND_INT64 : GrB_SECOND_INT32 ;
-    GrB_Semiring semiring  = NULL;
-    GrB_IndexUnaryOp ramp = NULL ;
+    // create a sparse boolean vector frontier, and set frontier(src) = true
+    GRB_TRY (GrB_Vector_new(&frontier, GrB_BOOL, n)) ;
+    GRB_TRY (GrB_Vector_setElement(frontier, true, src)) ;
 
-    if (compute_parent)
-    {
-        // create the parent vector.  l_parent(i) is the parent id of node i
-        GRB_TRY (GrB_Vector_new(&l_parent, int_type, n)) ;
-
-        semiring = (n > INT32_MAX) ?
-            GrB_MIN_FIRST_SEMIRING_INT64 : GrB_MIN_FIRST_SEMIRING_INT32;
-
-        // create a sparse integer vector frontier, and set frontier(src) = src
-        GRB_TRY (GrB_Vector_new(&frontier, int_type, n)) ;
-        GRB_TRY (GrB_Vector_setElement(frontier, src, src)) ;
-
-        // pick the ramp operator
-        ramp = (n > INT32_MAX) ? GrB_ROWINDEX_INT64 : GrB_ROWINDEX_INT32 ;
-    }
-    else
-    {
-        // only the level is needed
-        semiring = LAGraph_structural_bool ;
-
-        // create a sparse boolean vector frontier, and set frontier(src) = true
-        GRB_TRY (GrB_Vector_new(&frontier, GrB_BOOL, n)) ;
-        GRB_TRY (GrB_Vector_setElement(frontier, true, src)) ;
-    }
-
-    if (compute_level)
-    {
-        // create the level vector. v(i) is the level of node i
-        // v (src) = 0 denotes the source node
-        GRB_TRY (GrB_Vector_new(&l_level, int_type, n)) ;
-        //GRB_TRY (GrB_Vector_setElement(l_level, 0, src)) ;
-    }
+    GRB_TRY (GrB_Vector_new(&l_level, int_type, n)) ;
 
     //--------------------------------------------------------------------------
     // BFS traversal and label the nodes
     //--------------------------------------------------------------------------
     GrB_Index nq = 1 ;          // number of nodes in the current level
     GrB_Index last_nq = 0 ;
-    GrB_Index current_level = 0;
+    GrB_Index current_level = 1;
     GrB_Index nvals = 1;
 
     // {!mask} is the set of unvisited nodes
-    GrB_Vector mask = (compute_parent) ? l_parent : l_level ;
+    GrB_Vector mask = l_level ;
 
     // parent BFS
     do
     {
-        if (compute_level)
-        {
-            // assign levels: l_level<s(frontier)> = current_level
-            GRB_TRY( GrB_assign(l_level, frontier, GrB_NULL,
-                                current_level, GrB_ALL, n, GrB_DESC_S) );
-            ++current_level;
-        }
-
-        if (compute_parent)
-        {
-            // frontier(i) currently contains the parent id of node i in tree.
-            // l_parent<s(frontier)> = frontier
-            GRB_TRY( GrB_assign(l_parent, frontier, GrB_NULL,
-                                frontier, GrB_ALL, n, GrB_DESC_S) );
-
-            // convert all stored values in frontier to their indices
-            GRB_TRY (GrB_apply (frontier, GrB_NULL, GrB_NULL, ramp,
-                frontier, 0, GrB_NULL)) ;
-        }
-
+        // assign levels: l_level<s(frontier)> = current_level
+        GRB_TRY( GrB_assign(l_level, frontier, GrB_NULL,
+                            current_level, GrB_ALL, n, GrB_DESC_S) );
+        ++current_level;
         // frontier = kth level of the BFS
         // mask is l_parent if computing parent, l_level if computing just level
-        GRB_TRY( GrB_vxm(frontier, mask, GrB_NULL, semiring,
+        //WARNING! USING USER-VERSION OF A SEMIRING
+        GRB_TRY( GrB_vxm(frontier, mask, GrB_NULL, lablas::LogicalOrAndSemiring<bool>(),
                          frontier, A, GrB_DESC_RSC) );
 
         // done if frontier is empty
@@ -158,8 +109,6 @@ int LG_BreadthFirstSearch_vanilla
     // free workspace and return result
     //--------------------------------------------------------------------------
 
-    if (compute_parent) (*parent) = l_parent ;
-    if (compute_level ) (*level ) = l_level ;
-    LG_FREE_WORK ;
+    (*level ) = l_level ;
     return (GrB_SUCCESS) ;
 }
